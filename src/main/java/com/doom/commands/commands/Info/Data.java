166644fb819e6498dce1a0550d68f10028971a69
Doom306 <keenebrigado@gmail.com>
2021-02-09 07:37:51 +0800

    wowowowo

--- src/main/java/com/doom/commands/commands/Info/Data.java
+++ src/main/java/com/doom/commands/commands/Info/Data.java
@@ -1,4 +1,12 @@
 package com.doom.commands.commands.Info;
 
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.User;
+
+import java.util.HashMap;
+
 public class Data {
+
+    public static HashMap<Guild, String> message = new HashMap<>();
+    public static HashMap<User, Guild> user = new HashMap<>();
 }
